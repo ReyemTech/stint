3b3a4a8dcb9524a7ac5fd22d6fa3a4e28cf5f0bf
Mario Meyer <mariomeyer@gmail.com>
2026-05-18 20:55:14 -0400

    fix(ui): use HashRouter so /#/route URLs match

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,14 +1,15 @@
-import { Route, Router } from "@solidjs/router";
+import { HashRouter, Route } from "@solidjs/router";
 import Popover from "./routes/Popover";
 import Settings from "./routes/Settings";
 import Today from "./routes/Today";
 
 export default function App() {
   return (
-    <Router>
+    <HashRouter>
       <Route path="/popover" component={Popover} />
       <Route path="/today" component={Today} />
       <Route path="/settings" component={Settings} />
-    </Router>
+      <Route path="*" component={Today} />
+    </HashRouter>
   );
 }
